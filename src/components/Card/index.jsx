30885a03958a40cd82f309d764d645c7be78f51c
AlexSuarezRodriguez <alex.suarez@hotmail.com>
2022-06-24 21:31:11 -0500

    scrollInfinite comics

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -4,16 +4,20 @@ import { PERSONAJE_DETAILS } from "../../Constants/Routes";
 import "./card.css";
 
 function Card({ image, id, name, description, title }) {
-  const descrip = description.slice(0, 60);
   return (
     <div className="container-card">
       <img src={`${image.path}.${image.extension}`} alt="12" />
       <div className="container-card_paragraph">
-        {name ? <h3>{name}</h3> : <h3>{title}</h3>}
+        {name ? <h3><strong>{name}</strong></h3> : <h3><strong>{title}</strong></h3>}
+        {description && (
+          <p style={{ fontSize: "10px" }}>{description.slice(0, 60)}...</p>
+        )}
+        <div>
 
-        <p style={{ fontSize: "10px" }}>{descrip}...</p>
-
-        <Link to={`${PERSONAJE_DETAILS}/${id}`}>Go details</Link>
+        <Link to={`${PERSONAJE_DETAILS}/${id}`} className="btn-header-users">
+          Go details
+        </Link>
+        </div>
       </div>
     </div>
   );
@@ -23,6 +27,6 @@ export default Card;
 
 Card.defaultProps = {
   name: "",
-  description: "",
+  description: "...",
   title: "",
 };
--- src/page/PersonajeDetails/index.jsx
+++ src/page/PersonajeDetails/index.jsx
@@ -43,47 +43,50 @@ function PersonajeDetails() {
   }, []);
   console.log(comics);
   return (
-    <>
+    <div style={{display:'flex', justifyContent:'center'}}>
       {personaje.length > 0 && (
         <div className="containerDetails">
           <img
             src={`${personaje[0].thumbnail.path}.${personaje[0].thumbnail.extension}`}
             alt="qw"
           />
-          <div className="containerDetails_description">
-            <h2>{personaje[0].name}</h2>
-            {/* {personaje[0].description ? <p>{personaje[0].description}</p>:'...'} */}
-            
-            <div>
-              {/* {comics.length > 0 && ( */}
-              <InfiniteScroll
-                className="contenedorapp"
-                dataLength={comics.length} //This is important field to render the next data
-                next={fetchDataComics}
-                hasMore={hashMore}
-                loader={<h4>Loading...</h4>}
-                endMessage={
-                  <p style={{ textAlign: "center" }}>
-                    <b>Yay! You have seen it all</b>
-                  </p>
-                }
-              >
-                {comics.map((element) => (
-                  <Card
-                    key={element.id}
-                    image={element.thumbnail}
-                    name={element.name}
-                    description={element.description}
-                    id={element.id}
-                  />
-                ))}
-              </InfiniteScroll>
-              {/* )} */}
-            </div>
+          <div className="containerDetails_description" id="scroll">
+            <h1 style={{margin: '0 auto', fontSize:'25px'}}><strong>{personaje[0].name}</strong></h1>
+            {personaje[0].description ? (
+              <p>{personaje[0].description}</p>
+            ) : (
+              "..."
+            )}
+
+            {/* {comics.length > 0 && ( */}
+            <InfiniteScroll
+              className="contenedorapp"
+              scrollableTarget="scroll"
+              dataLength={comics.length} //This is important field to render the next data
+              next={fetchDataComics}
+              hasMore={hashMore}
+              loader={<h4>Loading...</h4>}
+              endMessage={
+                <p style={{ textAlign: "center" }}>
+                  <b>Yay! You have seen it all</b>
+                </p>
+              }
+            >
+              {comics.map((element) => (
+                <Card
+                  key={element.id}
+                  image={element.thumbnail}
+                  title={element.title}
+                  description={element.description}
+                  id={element.id}
+                />
+              ))}
+            </InfiniteScroll>
+            {/* )} */}
           </div>
         </div>
       )}
-    </>
+    </div>
   );
 }
 
